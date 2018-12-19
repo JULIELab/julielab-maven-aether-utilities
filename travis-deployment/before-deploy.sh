15f1f7b8e793426f5044d58e96bd52bea0d6e4fc
khituras <chew@gmx.net>
2018-12-19 14:09:34 +0100

    Bug fix in deployment script

--- travis-deployment/before-deploy.sh
+++ travis-deployment/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	openssl aes-256-cbc -K $encrypted_a53075a80cca_key -iv $encrypted_a53075a80cca_iv -in codesigning.asc.enc -out codesigning.asc -d
-	gpg --fast-import cd/codesigning.asc
+	gpg --fast-import travis-deployment/codesigning.asc
 fi
 
