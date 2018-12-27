aa06e54a4f44774f85a3faa0f2f980b279310854
khituras <chew@gmx.net>
2018-12-27 15:40:37 +0100

    Version 1.0.0

--- src/main/java/de/julielab/utilities/aether/MavenProjectUtilities.java
+++ src/main/java/de/julielab/utilities/aether/MavenProjectUtilities.java
@@ -2,12 +2,7 @@ package de.julielab.utilities.aether;
 
 import org.apache.maven.model.Model;
 import org.apache.maven.model.building.*;
-import org.apache.maven.model.io.DefaultModelReader;
-import org.apache.maven.model.normalization.DefaultModelNormalizer;
-import org.apache.maven.model.profile.DefaultProfileSelector;
 import org.apache.maven.model.resolution.ModelResolver;
-import org.apache.maven.model.superpom.DefaultSuperPomProvider;
-import org.apache.maven.model.validation.DefaultModelValidator;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.apache.maven.settings.building.SettingsBuildingException;
 import org.eclipse.aether.RepositorySystemSession;
@@ -15,12 +10,13 @@ import org.eclipse.aether.impl.ArtifactResolver;
 import org.eclipse.aether.impl.DefaultServiceLocator;
 import org.eclipse.aether.impl.RemoteRepositoryManager;
 import org.eclipse.aether.impl.VersionRangeResolver;
-import org.eclipse.aether.spi.locator.Service;
-import org.eclipse.aether.spi.locator.ServiceLocator;
 
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MavenProjectUtilities {
 
@@ -28,10 +24,34 @@ public class MavenProjectUtilities {
     }
 
 
-    public static Model getRawPomModel(File pom) throws MavenException {
+    public static List<String> getProjectModules(File pom, boolean recursively) {
+        List<String> ret = new ArrayList<>();
+        final Model model = getRawPomModel(pom);
+        final List<String> modules = model.getModules();
+        if (modules != null) {
+            String basepathString = pom.getParent() != null ? pom.getParent() + File.separator : "";
+            ret.addAll(modules.stream().map(m -> basepathString + m).collect(Collectors.toList()));
+        }
+        if (recursively) {
+            ret.addAll(getRecursiveProjectModules(ret));
+        }
+        return ret;
+    }
+
+    public static List<String> getRecursiveProjectModules(List<String> modules) {
+        List<String> ret = new ArrayList<>();
+        for (String module : modules) {
+            ret.addAll(getProjectModules(new File( module + File.separator + "pom.xml"), true));
+        }
+        return ret;
+    }
+
+    public static Model getRawPomModel(File pom) {
         final DefaultModelBuilder modelBuilder = new DefaultModelBuilderFactory().newInstance();
         final Result<? extends Model> result = modelBuilder.buildRawModel(pom, 0, false);
         final Model model = result.get();
+        if (model == null)
+            throw new IllegalArgumentException("Could not create a model from file " + pom.getAbsolutePath());
         return model;
     }
 
