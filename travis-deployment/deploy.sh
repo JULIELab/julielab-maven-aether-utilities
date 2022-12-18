d33c4781b24264033a49ea86aefc1ff49f55f13a
khituras <chew@gmx.net>
2022-12-18 15:56:39 +0100

    Remove travis references because we can't use it any more.

--- travis-deployment/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	echo "Executing deploy"
-    mvn deploy -P sonatype-nexus-deployment --settings travis-deployment/mvnsettings.xml -DskipTests=true
-else
-	echo "Deploy not executed"
-fi
